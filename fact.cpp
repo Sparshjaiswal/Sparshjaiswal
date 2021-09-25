fb44c28562f853ce463e54fb1289603667c7f10c
Sparshjaiswal <91361949+Sparshjaiswal@users.noreply.github.com>
2021-09-25 12:32:37 +0530

    Add files via upload

---
+++ fact.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fac(int n)
+{
+	if(n<=1)
+	{	
+	return 1;
+	}
+	return n*fac(n-1);
+}
+	int main()
+{
+	int a;
+	cout<<"enter the factorial 0f";
+	cin>>a;
+	cout<<"factorial of "<<a<<" is:"<<fac(a);
+	return 0;
+}
