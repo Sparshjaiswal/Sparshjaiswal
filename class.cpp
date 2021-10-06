8d091dc2449a83ddc04e7497e218eeebced190cb
Sparshjaiswal <91361949+Sparshjaiswal@users.noreply.github.com>
2021-10-06 10:54:02 +0530

    Add files via upload
    sparsh jaiswal 20011063 k

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class student
+{
+	public:
+	int id;
+	string name;
+};
+int main()
+{
+	student s1;
+	s1.id = 2001;
+	s1.name = "Sparsh jaiswal;
+	cout<<s1.name<<endl;
+	return 0;
+}
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
